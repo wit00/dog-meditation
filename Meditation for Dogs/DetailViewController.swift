e947f31a668b027b6ca32e0e8cdf7ffda0faff17
Whitney Powell <whitney@whitneys-macbook-pro.local>
2016-03-31 19:05:34 -0400

    Initial Commit

---
+++ Meditation for Dogs/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Meditation for Dogs
+//
+//  Created by Whitney Powell on 3/31/16.
+//  Copyright © 2016 Whitney Powell. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.valueForKey("timeStamp")!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Meditation for DogsTests/Meditation_for_DogsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Meditation_for_DogsTests.swift
+//  Meditation for DogsTests
+//
+//  Created by Whitney Powell on 3/31/16.
+//  Copyright © 2016 Whitney Powell. All rights reserved.
+//
+
+import XCTest
+@testable import Meditation_for_Dogs
+
+class Meditation_for_DogsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
